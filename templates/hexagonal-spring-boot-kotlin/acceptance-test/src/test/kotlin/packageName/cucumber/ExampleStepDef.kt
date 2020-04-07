5ab173d8be4c08eac5e93ce14df97b8e9cec8ea2
sundardamon <sundarkannan96@gmail.com>
2020-04-07 13:44:21 +0530

    fix(template-kotlin): fixed review comments

--- templates/hexagonal-spring-boot-kotlin/acceptance-test/src/test/kotlin/packageName/cucumber/ExampleStepDef.kt
+++ templates/hexagonal-spring-boot-kotlin/acceptance-test/src/test/kotlin/packageName/cucumber/ExampleStepDef.kt
@@ -1,14 +1,13 @@
 package packageName.cucumber
 
-import org.assertj.core.api.Assertions.assertThat
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
-
 import io.cucumber.datatable.DataTable
 import io.cucumber.java8.En
 import io.cucumber.java8.HookNoArgsBody
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootContextLoader
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpStatus
@@ -25,7 +24,7 @@ import packageName.repository.entity.ExampleEntity
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(classes = [ExampleE2EApplication::class], webEnvironment = RANDOM_PORT)
 @ContextConfiguration(classes = [ExampleE2EApplication::class], loader = SpringBootContextLoader::class)
-class ExampleStepDef(restTemplate: TestRestTemplate, poetryDao: ExampleDao) : En {
+class ExampleStepDef(restTemplate: TestRestTemplate, exampleDao: ExampleDao) : En {
 
     companion object {
         private const val LOCALHOST = "http://localhost:"
@@ -34,19 +33,19 @@ class ExampleStepDef(restTemplate: TestRestTemplate, poetryDao: ExampleDao) : En
 
     @LocalServerPort
     private val port: Int = 0
-    private var responseEntity: ResponseEntity<ExampleInfo>? = null
+    private lateinit var responseEntity: ResponseEntity<ExampleInfo>
 
     init {
 
-        DataTableType { row: Map<String, String> -> Example(1L, row["description"]) }
-        DataTableType { row: Map<String, String> -> ExampleEntity(1L, row["description"]) }
+        DataTableType { row: Map<String, String> -> Example(1L, row["description"].toString()) }
+        DataTableType { row: Map<String, String> -> ExampleEntity(1L, row["description"].toString()) }
 
-        Before(HookNoArgsBody { poetryDao.deleteAll() })
-        After(HookNoArgsBody { poetryDao.deleteAll() })
+        Before(HookNoArgsBody { exampleDao.deleteAll() })
+        After(HookNoArgsBody { exampleDao.deleteAll() })
 
         Given("the following examples exists in the library") { dataTable: DataTable ->
-            val poems = dataTable.asList<ExampleEntity>(ExampleEntity::class.java)
-            poetryDao.saveAll(poems)
+            val examples = dataTable.asList<ExampleEntity>(ExampleEntity::class.java)
+            exampleDao.saveAll(examples)
         }
 
         When("user requests for all examples") {
@@ -55,11 +54,11 @@ class ExampleStepDef(restTemplate: TestRestTemplate, poetryDao: ExampleDao) : En
         }
 
         Then("the user gets the following examples") { dataTable: DataTable ->
-            val expectedExampleInfo = dataTable.asList<Example>(Example::class.java)
-            assertThat(responseEntity!!.statusCode).isEqualTo(HttpStatus.OK)
-            assertThat(responseEntity!!.body).isNotNull
-            assertThat(responseEntity!!.body!!.examples).isNotEmpty.extracting("description")
-                    .contains(*expectedExampleInfo.stream().map(Example::description).toArray())
+            val expectedExamples = dataTable.asList<Example>(Example::class.java)
+            assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)
+            assertThat(responseEntity.body).isNotNull
+            assertThat(responseEntity.body.examples).isNotEmpty.extracting("description")
+                    .contains(*expectedExamples.map { it.description }.toTypedArray())
         }
     }
 }
